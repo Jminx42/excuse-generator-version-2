84d9675783408a85cd3d08abbe943f2864133087
Jminx42 <visagiejessica@gmail.com>
2023-03-17 19:56:18 +0000

    finished excuse generator with random excuse generated on load of window

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ["The dog", "My grandma", "His turtle", "My bird", "Her llama"];
+let action = ["ate", "peed", "crushed", "broke", "stole"];
+let what = ["my homework", "the keys", "the car", "the trampoline"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+  "once upon a time"
+];
+
+const randomItemOfArray = arr => arr[Math.floor(Math.random() * arr.length)];
+const randomExcuse =
+  randomItemOfArray(who) +
+  " " +
+  randomItemOfArray(action) +
+  " " +
+  randomItemOfArray(what) +
+  " " +
+  randomItemOfArray(when);
+window.onload = () => {
+  let excuse = document.getElementById("excuse");
+  return (excuse.innerHTML = randomExcuse);
 };
